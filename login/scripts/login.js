ab59b03347a672514b24162ca2b0fcef73e703a8
Pankaj Agarwal <98219223+PankajAgarwalS@users.noreply.github.com>
2023-02-23 14:05:03 +0530

    Update login.js

--- login/scripts/login.js
+++ login/scripts/login.js
@@ -3,10 +3,10 @@ function check(form) {
     password = new Array("admin1", "admin2", "members");
 
 	if(form.userid.value == username[0] && form.pswrd.value == password[0]||form.userid.value == username[1] && form.pswrd.value == password[1]) {
-		location.replace('#adminhome')/* The Administrator homepage */
+		location.replace('admin/admiin.html')/* The Administrator homepage */
 	}
 	else if(form.userid.value == username[2] && form.pswrd.value == password[2]) {
-		location.replace('members/home.html')/* The members homepage */
+		location.replace('member/member.html')/* The members homepage */
 	}
 	else if(form.userid.value == username[0]||form.userid.value == username[1]||form.userid.value == username[2]) {
 		alert("Incorrect Password")
